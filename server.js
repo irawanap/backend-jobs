2f37419735d1a2eeef5e15306144dd6db3fb8e9a
irawanap <irawanajhi22@student.ub.ac.id>
2025-08-02 16:10:49 +0700

    Update server.js: refactor middleware usage and improve server structure

--- server.js
+++ server.js
@@ -2,18 +2,21 @@ const express = require('express');
 const router = require('./routes/userRoute');
 const app = express()
 const bodyParser = require('body-parser')
+const logger = require('./middleware/logger')
 
 const port = 5000;
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(logger);
 
-app.use(bodyParser.json)
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello world!')
 })
 
-app.use('/api', router)
+app.use('/api', router);
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
