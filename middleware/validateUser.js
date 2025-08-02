f5acf0c08fb4ce2b8562379e26ef5ad01d3f5608
irawanap <irawanajhi22@student.ub.ac.id>
2025-08-02 18:48:07 +0700

    Add: validateUser middleware

---
+++ middleware/validateUser.js
@@ -0,0 +1,23 @@
+const { check, validationResult } = require('express-validator');
+
+const validateUser = [
+    check('email').isEmail().withMessage('Email is invalid'),
+    check('password')
+        .isLength({ min:6 })
+        .withMessage('Password is invalid'),
+    check('name').notEmpty().withMessage('Name is invalid'),
+
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(422).json({
+                success: false,
+                message: "Validation error",
+                errors: errors.array()
+            });
+        }
+        next();
+    }
+];
+
+module.exports = { validateUser };
