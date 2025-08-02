dcdfc868661775b7f19b81bb024e5b60e99d236c
irawanap <irawanajhi22@student.ub.ac.id>
2025-08-02 16:10:59 +0700

    Add logger middleware: log request method and URL with response time

---
+++ middleware/logger.js
@@ -0,0 +1,11 @@
+const logger = (req, res, next) => {
+    const path = req.originalUrl.split('?')[0]; 
+    const label = `${req.method} ${path}`;
+
+    res.on("finish", () => {
+        console.timeEnd(label);
+    });
+    next();
+};
+
+module.exports = logger;
