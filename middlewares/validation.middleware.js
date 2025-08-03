9a34f75786a2ecd6f54f1dee960f974c6538a87a
irawanap <irawanajhi22@student.ub.ac.id>
2025-08-04 00:39:20 +0700

    refactor: enhance validation logic and error messages

--- middlewares/validation.middleware.js
+++ middlewares/validation.middleware.js
@@ -30,6 +30,17 @@ const validate = (type) => {
                 check('email').isEmail().withMessage('Email is invalid'),
                 check('password').notEmpty().withMessage('Password is required')
             ]);
+        case "createJob": // Aturan validasi untuk membuat pekerjaan baru
+            return handleValidation([
+                check("company").notEmpty().withMessage("Company is required"),
+                check("position")
+                .notEmpty()
+                .withMessage("Position is required"),
+                // menambahkan validasi untuk status jika ingin membatasi nilai enum
+                // check('status').isIn(['applied', 'interview', 'offer', 'rejected']).withMessage('Invalid job status'),
+                // check('appliedDate').isISO8601().toDate().withMessage('Invalid applied date format (YYYY-MM-DD)'),
+                // check('offerDate').optional().isISO8601().toDate().withMessage('Invalid offer date format (YYYY-MM-DD)'),
+            ]);
         case 'getAllUsers':
             return handleValidation([
                 // Tambahkan validasi jika perlu, misal query param
