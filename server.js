7e54b95254e5fc7180f511c7504504b74b60759b
irawanap <irawanajhi22@student.ub.ac.id>
2025-08-03 16:31:47 +0700

    refactor: remove old user controller, middleware, and routes

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
-const express = require('express');
-const router = require('./routes/userRoute');
-const app = express()
-const bodyParser = require('body-parser')
-const logger = require('./utils/logger')
+require('dotenv').config();
 
-const port = 5000;
+const express = require('express');
+const app = express();
+const authRoutes = require('./routes/auth.route');
+const userRoutes = require('./routes/user.route');
+const bodyParser = require('body-parser');
+const logger = require('./utils/logger');
+
+const port = process.env.PORT || 5000
 
 app.use(logger);
 
@@ -16,7 +19,13 @@ app.get('/', (req, res) => {
     res.send('Hello world!')
 })
 
-app.use('/api', router);
+app.use('/api/auth', authRoutes);
+app.use('/api/users', userRoutes);
+
+app.use((err, req, res, next) => {
+  console.error(err.stack) // Log the error stack for debugging
+  res.status(500).send("Something broke!")
+})
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
--- services/user.service.js
+++ services/user.service.js
@@ -1,19 +1,34 @@
 const prisma = require('../model/prisma');
-const bcrypt = require('bcryptjs');
 
+// Get All Users
 exports.getAllUsers = async () => {
-    return await prisma.user.findMany();
-}
-
-exports.createUser = async ({ name, email, password }) => {
-
-    const hashedPassword = await bcrypt.hash(password, 6);
-
-    return await prisma.user.create({
-        data: {
-            email,
-            name,
-            password: hashedPassword,
-        },
+    return await prisma.user.findMany({
+        select: {
+            id: true,
+            name: true,
+            email: true
+        }
     });
-}
+};
+
+// // Get User By ID
+// exports.findUserById = async (id) => {
+//     return await prisma.user.findUnique({
+//         where: { id }
+//     });
+// };
+
+// // Update User By ID
+// exports.updateUser = async (id, newUserData) => {
+//     return await prisma.user.update({
+//         where: { id },
+//         data: newUserData
+//     });
+// };
+
+// // Delete User By ID
+// exports.deleteUser = async (id) => {
+//     return await prisma.user.delete({
+//         where: { id }
+//     });
+// };
